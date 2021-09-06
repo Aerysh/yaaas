b5f144dc52c960c1563524a747736eea7749e763
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2021-09-06 16:24:10 +0700

    Update all change puppeteer to axios

--- routes/all.js
+++ routes/all.js
@@ -1,25 +1,13 @@
 const express = require('express');
-const _browser = require('../helpers/puppeteer');
 const cheerio = require('cheerio');
 const router = express.Router();
 const { all } = require('../helpers/url');
+const axios = require('axios');
 
 router.get('/:page', async (req, res) => {
     try{
-        const browser = await _browser();
-        const page = await browser.newPage();
-        await page.setRequestInterception(true);
-        page.on('request', (req) => {
-            if(req.resourceType() === 'image' || req.resourceType() === 'stylesheet' || req.resourceType() === 'font'){
-                req.abort();
-            }else{
-                req.continue();
-            }
-        });
-        await page.goto(all + req.params.page);
-        const content = await page.content();
-
-        const $ = cheerio.load(content);
+        const { data } = await axios.get(all + req.params.page);
+        const $ = cheerio.load(data);
         const manhwaList = $(".postbody .mrgn .listupd .bs");
         const manhwas = [];
         manhwaList.each((idx, el) => {
@@ -31,8 +19,6 @@ router.get('/:page', async (req, res) => {
 
             manhwas.push(manhwa);
         });
-        await page.close();
-        await browser.close();
 
         res.json({message: "All Manhwa List", manhwas: manhwas});
     } catch(err) {
