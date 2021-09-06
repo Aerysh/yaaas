4bc1d148f88aee9c4506d0a8ad5136b44245bc2a
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2021-09-06 16:12:32 +0700

    Update latest replace puppeteer to axios

--- routes/latest.js
+++ routes/latest.js
@@ -3,22 +3,12 @@ const _browser = require('../helpers/puppeteer');
 const cheerio = require('cheerio');
 const router = express.Router();
 const { latest } = require('../helpers/url');
+const axios = require('axios');
 
 router.get('/', async (req, res) => {
     try{
-        const browser = await _browser();
-        const page = await browser.newPage();
-        await page.setRequestInterception(true);
-        page.on('request', (req) => {
-            if(req.resourceType() === 'image' || req.resourceType() === 'stylesheet' || req.resourceType() === 'font'){
-                req.abort();
-            }else{
-                req.continue();
-            }
-        });
-        await page.goto(latest);
-        const content = await page.content();
-        const $ = cheerio.load(content);
+        const { data } = await axios.get(latest);
+        const $ = cheerio.load(data);
         const latestUpdate = $(".listupd .bs");
         const manhwas = [];
         latestUpdate.each((idx, el) => {
@@ -30,8 +20,6 @@ router.get('/', async (req, res) => {
             
             manhwas.push(manhwa);
         });
-        await page.close();
-        await browser.close();
         
         res.json({message: "Latest Update Manhwas", manhwas: manhwas});
     } catch(err) {
