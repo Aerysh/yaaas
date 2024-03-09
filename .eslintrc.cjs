a9365cb6966b8679dd2e480155d356ac6800844f
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-03-09 11:59:59 +0700

    chore: Update ESLint and Prettier configurations
    - Updated .eslintrc.cjs with the recommended configuration to include 'plugin:prettier/recommended'.
    - Added .prettierignore to specify files and directories to be ignored by Prettier.
    - Added .prettierrc with basic Prettier configuration settings.
    - Updated package.json scripts:
      - Added "format" script for running Prettier to format code.
      - Added "lint" script for running ESLint to lint code.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,23 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
