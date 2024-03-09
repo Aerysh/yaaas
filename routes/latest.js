6e752b22fbdea5f0338e03e86cac868b5bdb611b
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-03-09 12:01:43 +0700

    refactor: Enhance code style consistency and readability

--- routes/latest.js
+++ routes/latest.js
@@ -1,37 +1,40 @@
-import express from "express";
-import cheerio from "cheerio";
-import Browser from "../helpers/puppeteer.js";
-import UrlHelper from "../helpers/url.js";
+import express from 'express';
+import cheerio from 'cheerio';
+import Browser from '../helpers/puppeteer.js';
+import UrlHelper from '../helpers/url.js';
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-	try{
-		const browser = await Browser();
-		const page = await browser.newPage();
-		await page.goto(UrlHelper.latest);
-		const content = await page.content();
+router.get('/', async (req, res) => {
+  try {
+    const browser = await Browser();
+    const page = await browser.newPage();
+    await page.goto(UrlHelper.latest);
+    const content = await page.content();
 
-		const $ = cheerio.load(content);
-		const latestUpdate = $(".listupd .bs");
-		const manhwas = [];
-		latestUpdate.each((idx, el) => {
-			const manhwa = {title: "", chapter: "", thumbnail: "", endpoint: ""};
-			manhwa.title = $(el).find(".bsx a").attr("title");
-			manhwa.chapter = $(el).find(".bsx a .bigor .adds .epxs").text();
-			manhwa.thumbnail = $(el).find(".bsx a .limit img").attr("src");
-			manhwa.endpoint = $(el).find(".bsx a").attr("href").replace("https://manhwaindo.id/series/", "");
-            
-			manhwas.push(manhwa);
-		});
+    const $ = cheerio.load(content);
+    const latestUpdate = $('.listupd .bs');
+    const manhwas = [];
+    latestUpdate.each((idx, el) => {
+      const manhwa = { title: '', chapter: '', thumbnail: '', endpoint: '' };
+      manhwa.title = $(el).find('.bsx a').attr('title');
+      manhwa.chapter = $(el).find('.bsx a .bigor .adds .epxs').text();
+      manhwa.thumbnail = $(el).find('.bsx a .limit img').attr('src');
+      manhwa.endpoint = $(el)
+        .find('.bsx a')
+        .attr('href')
+        .replace('https://manhwaindo.id/series/', '');
 
-		await page.close();
-		await browser.close();
-        
-		res.json({message: "Latest Update Manhwas", manhwas: manhwas});
-	} catch(err) {
-		res.json({message: err});
-	}
+      manhwas.push(manhwa);
+    });
+
+    await page.close();
+    await browser.close();
+
+    res.json({ message: 'Latest Update Manhwas', manhwas: manhwas });
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 export default router;
--- routes/search.js
+++ routes/search.js
@@ -1,37 +1,40 @@
-import express from "express";
-import cheerio from "cheerio";
-import Browser from "../helpers/puppeteer.js";
-import UrlHelper from "../helpers/url.js";
+import express from 'express';
+import cheerio from 'cheerio';
+import Browser from '../helpers/puppeteer.js';
+import UrlHelper from '../helpers/url.js';
 
 const router = express.Router();
-router.get("/:title", async (req, res) => {
-	try{
-		const browser = await Browser();
-		const page = await browser.newPage();
-		await page.goto(UrlHelper.search + req.params.title);
-		const content = await page.content();
+router.get('/:title', async (req, res) => {
+  try {
+    const browser = await Browser();
+    const page = await browser.newPage();
+    await page.goto(UrlHelper.search + req.params.title);
+    const content = await page.content();
 
-		const $ = cheerio.load(content);
-		const manhwaList = $(".postbody .listupd .bs");
-		const manhwas = [];
-		manhwaList.each((idx, el) => {
-			const manhwa = {title: "", thumbnail: "", latest_chapter: "", endpoint: ""};
-			manhwa.title = $(el).find(".bsx a").attr("title");
-			manhwa.thumbnail = $(el).find(".bsx a .limit img").attr("src"); 
-			manhwa.latest_chapter = $(el).find(".bsx a .bigor .adds .epxs").text();
-			manhwa.endpoint = $(el).find(".bsx a").attr("href").replace("https://manhwaindo.id/series/", "");
+    const $ = cheerio.load(content);
+    const manhwaList = $('.postbody .listupd .bs');
+    const manhwas = [];
+    manhwaList.each((idx, el) => {
+      const manhwa = { title: '', thumbnail: '', latest_chapter: '', endpoint: '' };
+      manhwa.title = $(el).find('.bsx a').attr('title');
+      manhwa.thumbnail = $(el).find('.bsx a .limit img').attr('src');
+      manhwa.latest_chapter = $(el).find('.bsx a .bigor .adds .epxs').text();
+      manhwa.endpoint = $(el)
+        .find('.bsx a')
+        .attr('href')
+        .replace('https://manhwaindo.id/series/', '');
 
-			manhwas.push(manhwa);
-		});
+      manhwas.push(manhwa);
+    });
 
-		await page.close();
-		await browser.close();
+    await page.close();
+    await browser.close();
 
-		res.json({message: "Search Manhwa Results", manhwas: manhwas});
-	} catch(err) {
-		console.log(err);
-		res.json({message: err});
-	}
+    res.json({ message: 'Search Manhwa Results', manhwas: manhwas });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: err });
+  }
 });
 
 export default router;
