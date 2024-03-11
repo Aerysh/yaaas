c1953d8db0c512540cca2e1f60d0a80391e0e063
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-03-11 20:29:14 +0700

    refactor: apply new prettier code format

--- src/routes/all.js
+++ src/routes/all.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cheerio from 'cheerio';
-import Browser from '../utils/puppeteer.js';
-import UrlHelper from '../utils/url-helper.js';
+import express from "express";
+import cheerio from "cheerio";
+import Browser from "../utils/puppeteer.js";
+import UrlHelper from "../utils/url-helper.js";
 
 const router = express.Router();
 
-router.get('/:page', async (req, res) => {
+router.get("/:page", async (req, res) => {
   try {
     const browser = await Browser();
     const page = await browser.newPage();
@@ -13,17 +13,22 @@ router.get('/:page', async (req, res) => {
     const content = await page.content();
 
     const $ = cheerio.load(content);
-    const manhwaList = $('.postbody .bixbox .mrgn .listupd .bs');
+    const manhwaList = $(".postbody .bixbox .mrgn .listupd .bs");
     const manhwas = [];
     manhwaList.each((idx, el) => {
-      const manhwa = { title: '', thumbnail: '', latest_chapter: '', endpoint: '' };
-      manhwa.title = $(el).find('.bsx a').attr('title');
-      manhwa.thumbnail = $(el).find('.bsx a .limit img').attr('src');
-      manhwa.latest_chapter = $(el).find('.bsx a .bigor .adds .epxs').text();
+      const manhwa = {
+        title: "",
+        thumbnail: "",
+        latest_chapter: "",
+        endpoint: "",
+      };
+      manhwa.title = $(el).find(".bsx a").attr("title");
+      manhwa.thumbnail = $(el).find(".bsx a .limit img").attr("src");
+      manhwa.latest_chapter = $(el).find(".bsx a .bigor .adds .epxs").text();
       manhwa.endpoint = $(el)
-        .find('.bsx a')
-        .attr('href')
-        .replace('https://manhwaindo.id/series/', '');
+        .find(".bsx a")
+        .attr("href")
+        .replace("https://manhwaindo.id/series/", "");
 
       manhwas.push(manhwa);
     });
@@ -31,7 +36,7 @@ router.get('/:page', async (req, res) => {
     await page.close();
     await browser.close();
 
-    res.json({ message: 'All Manhwa List', manhwas: manhwas });
+    res.json({ message: "All Manhwa List", manhwas: manhwas });
   } catch (err) {
     res.json({ message: err });
   }
--- src/utils/puppeteer.js
+++ src/utils/puppeteer.js
@@ -1,12 +1,12 @@
-import puppeteer from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 const Browser = async () => {
   try {
     puppeteer.use(StealthPlugin());
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox'],
+      args: ["--no-sandbox"],
     });
     return browser;
   } catch (err) {
