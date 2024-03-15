f46fccb203de5909c8708953517e989f2619276c
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-03-15 19:28:25 +0700

    chore: update ESLint, Prettier, and package configuration
    - Updated ESLint configuration to include additional plugins and rules.
    - Updated Prettier configuration to change printWidth and singleQuote.
    - Added eslint-plugin-import to devDependencies in package.json.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
   overrides: [
     {
       env: {
@@ -19,5 +24,17 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "no-console": "warn", // Warn about console.log statements
+    "no-debugger": "error", // Prevent debugger statements
+    "no-var": "error", // Require let or const instead of var
+    "prefer-const": "error", // Prefer const over let
+    "no-unused-vars": "warn", // Warn about unused variables
+    "import/no-unresolved": "error", // Ensure imports point to a file/module that can be resolved
+    "import/named": "error", // Ensure named imports correspond to a named export in the remote file
+    "import/namespace": "error", // Ensure a default import is present, given a namespace import is used
+    "import/default": "error", // Ensure a default export is present, given a default import is used
+    "import/export": "error", // Report any invalid exports
+  },
+  plugins: ["import"],
 };
