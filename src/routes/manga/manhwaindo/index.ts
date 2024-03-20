10d5da6972efcd17e6774f151e02cb06747ce954
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-03-20 23:03:11 +0700

    feat(routes): Add search endpoint to Manhwaindo API
    - Added new route for searching manga titles in Manhwaindo.
    - Implemented functionality to fetch search results from the Manhwaindo website.
    - Added error handling for cases where no results are found or internal server errors occur.

--- src/routes/manga/manhwaindo/index.ts
+++ src/routes/manga/manhwaindo/index.ts
@@ -5,12 +5,14 @@ import ManhwaindoGenres from './genres';
 import ManhwaindoLatest from './latest';
 import ManhwaindoPopular from './popular';
 import ManhwaindoRead from './read';
+import ManhwaindoSearch from './search';
 
 const Manhwaindo = async (fastify: FastifyInstance) => {
   await fastify.register(ManhwaindoDetails, { prefix: '/details' });
   await fastify.register(ManhwaindoGenres, { prefix: '/genres' });
   await fastify.register(ManhwaindoLatest, { prefix: '/latest' });
   await fastify.register(ManhwaindoPopular, { prefix: '/popular' });
+  await fastify.register(ManhwaindoSearch, { prefix: '/search' });
   await fastify.register(ManhwaindoRead, { prefix: '/read' });
 
   fastify.get(
---
+++ src/routes/manga/manhwaindo/search.ts
@@ -0,0 +1,73 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+
+import launchBrowser from '../../../utils/puppeteer';
+
+import { Manhwa } from './types';
+import { manhwaindoUrlHelper } from './url-helper';
+
+const ManhwaindoSearch = async (fastify: FastifyInstance) => {
+  fastify.get<{ Params: { query: string } }>(
+    '/:query',
+    async (request: FastifyRequest<{ Params: { query: string } }>, reply: FastifyReply) => {
+      let browser;
+      let page;
+      try {
+        const encodedQuery = encodeURIComponent(request.params.query);
+
+        browser = await launchBrowser();
+        page = await browser.newPage();
+
+        await page.goto(manhwaindoUrlHelper.search(encodedQuery), {
+          waitUntil: 'networkidle0',
+        });
+
+        await page.waitForSelector('img', { timeout: 3000 });
+
+        const manhwas: Manhwa[] = await page.evaluate(() => {
+          const manhwaList = Array.from(
+            document.querySelectorAll('.postbody .bixbox .listupd .bs')
+          );
+          return manhwaList.map((el) => {
+            const title = el.querySelector('.bsx a')?.getAttribute('title') || '';
+            const thumbnail =
+              el.querySelector('.bsx a .limit img')?.getAttribute('data-lazy-src') ||
+              el.querySelector('.bsx a .limit img')?.getAttribute('src') ||
+              '';
+            const latestChapter = el.querySelector('.bsx a .bigor .adds .epxs')?.textContent || '';
+            let endpoint = el.querySelector('.bsx a')?.getAttribute('href') || '';
+            endpoint = endpoint
+              .replace('https://manhwaindo.id/series/', '')
+              .replace('https://manhwaindo.net/series/', '')
+              .replace('/', '');
+            return { title, thumbnail, latestChapter, endpoint };
+          });
+        });
+
+        if (manhwas.length === 0) {
+          reply.status(404).send({
+            message: 'Result not found',
+          });
+        } else {
+          reply.status(200).send({
+            message: `ManhwaIndo: Search Result for ${request.params.query}`,
+            manhwas,
+          });
+        }
+      } catch (error) {
+        reply.status(500).send({
+          message: 'Internal Server Error',
+          error,
+        });
+      } finally {
+        if (page) {
+          await page.close().catch(console.error);
+        }
+        if (browser) {
+          await browser.close().catch(console.error);
+        }
+      }
+    }
+  );
+};
+
+export default ManhwaindoSearch;
