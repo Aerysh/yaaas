d3b5064a0e6fc45c560e5297d1096679f74e2198
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-03-30 00:09:24 +0700

    refactor(utils): improve endpoint formatting

--- src/utils/format-endpoint.ts
+++ src/utils/format-endpoint.ts
@@ -1,5 +1,14 @@
 export default function formatEndpoint(endpoint: string): string {
-  const words = endpoint.replace(/-/g, ' ').split(' ');
-  const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+  const cleanedEndpoint = endpoint.replace(/[^a-zA-Z0-9 ]/g, ' ');
+
+  const words = cleanedEndpoint.split(' ');
+
+  const capitalizedWords = words.map((word) => {
+    if (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+    return word;
+  });
+
   return capitalizedWords.join(' ');
 }
