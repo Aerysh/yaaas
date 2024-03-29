b90698b3347af17189fb79d080c8972ef7a7885f
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-03-30 00:19:31 +0700

    feat(manga): add try-catch block to handle errors in ManhwaIndo provider

--- src/routes/manga/manhwaindo/index.ts
+++ src/routes/manga/manhwaindo/index.ts
@@ -25,33 +25,40 @@ const Manhwaindo = async (fastify: FastifyInstance) => {
       },
     },
     async (request: FastifyRequest, reply: FastifyReply) => {
-      reply.status(200).send({
-        message: `Welcome to ManhwaIndo Provider! Please support them by visiting their website at ${manhwaindoUrlHelper.base}. Thank you!`,
-        routes: {
-          '/details/:endpoint': {
-            description: 'Get details for a specific Manhwa.',
+      try {
+        reply.status(200).send({
+          message: `Welcome to ManhwaIndo Provider! Please support them by visiting their website at ${manhwaindoUrlHelper.base}. Thank you!`,
+          routes: {
+            '/details/:endpoint': {
+              description: 'Get details for a specific Manhwa.',
+            },
+            '/genres': {
+              description: 'Retrieve a list of all available genre.',
+            },
+            '/genres/:endpoint/:page': {
+              description:
+                'Get a list of Manhwa filtered by a specific genre, paginated by page number.',
+            },
+            '/latest/:page': {
+              description: 'Retrieve the latest updated Manhwa. paginated by page number.',
+            },
+            '/popular/:page': {
+              description: 'Retrieve the most popular Manhwa, paginated by page number.',
+            },
+            '/read/:endpoint': {
+              description: 'Read a specific chapter of a Manhwa.',
+            },
+            '/search/:query/:page': {
+              description: 'Search for a specific Manhwa. paginated by page number.',
+            },
           },
-          '/genres': {
-            description: 'Retrieve a list of all available genre.',
-          },
-          '/genres/:endpoint/:page': {
-            description:
-              'Get a list of Manhwa filtered by a specific genre, paginated by page number.',
-          },
-          '/latest/:page': {
-            description: 'Retrieve the latest updated Manhwa. paginated by page number.',
-          },
-          '/popular/:page': {
-            description: 'Retrieve the most popular Manhwa, paginated by page number.',
-          },
-          '/read/:endpoint': {
-            description: 'Read a specific chapter of a Manhwa.',
-          },
-          '/search/:query/:page': {
-            description: 'Search for a specific Manhwa. paginated by page number.',
-          },
-        },
-      });
+        });
+      } catch (error) {
+        reply.status(500).send({
+          message: 'An unexpected error occurred on the server.',
+          error,
+        });
+      }
     }
   );
 };
